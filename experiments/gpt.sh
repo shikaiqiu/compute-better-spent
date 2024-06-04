377cc9fca65f943aaeb2fe3aafd5d80be36a5ba5
shikai <sq2129@nyu.edu>
2024-06-03 21:27:50 -0400

    update scripts

--- experiments/gpt.sh
+++ experiments/gpt.sh
@@ -8,11 +8,16 @@ MAX_ITERS=600_000
 
 LR=2e-3
 
+#### BTT ####
 tt_rank=4
-for d_model in 1024 1536 2048 2560; do
+for d_model in 1024 1536 2048; do
 torchrun --nproc_per_node=8 --master_port=$(shuf -i 49152-65535 -n 1) train_gpt.py config/train_open.py --struct=btt_norm --layers=all --d_model=${d_model} --tt_rank=${tt_rank} --n_layer=12 --n_head=6 --d_head=64 --max_iters=${MAX_ITERS} --data_dir=${DATA_DIR} --out_dir=${OUT_DIR} --batch_size=${BATCH_SIZE} --gradient_accumulation_steps=${GRAD_ACCUM} --init_lr=${LR}
 done;
 
+d_model=2560
+torchrun --nproc_per_node=8 --master_port=$(shuf -i 49152-65535 -n 1) train_gpt.py config/train_open.py --struct=btt_norm --layers=all --d_model=${d_model} --tt_rank=${tt_rank} --n_layer=12 --n_head=12 --d_head=64 --max_iters=${MAX_ITERS} --data_dir=${DATA_DIR} --out_dir=${OUT_DIR} --batch_size=${BATCH_SIZE} --gradient_accumulation_steps=${GRAD_ACCUM} --init_lr=${LR}
+
+#### Dense ####
 for d_model in 384 512 768; do
 torchrun --nproc_per_node=8 --master_port=$(shuf -i 49152-65535 -n 1) train_gpt.py config/train_open.py --struct=dense --layers=all --d_model=${d_model} --n_layer=12 --n_head=-1 --d_head=64 --max_iters=${MAX_ITERS} --data_dir=${DATA_DIR} --out_dir=${OUT_DIR} --batch_size=${BATCH_SIZE} --gradient_accumulation_steps=${GRAD_ACCUM} --init_lr=${LR}
 done;
