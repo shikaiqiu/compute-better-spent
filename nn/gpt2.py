f26b33a1673e0c240af6e61aa987a29f2ba4e437
Shikai Qiu <46390122+shikaiqiu@users.noreply.github.com>
2024-06-05 20:18:18 -0400

    Update gpt2.py

--- nn/gpt2.py
+++ nn/gpt2.py
@@ -1,4 +1,6 @@
 """
+From https://github.com/karpathy/nanoGPT/tree/master
+
 Full definition of a GPT Language Model, all of it in this single file.
 References:
 1) the official GPT-2 TensorFlow implementation released by OpenAI:
